6d1884b226abc8c197b5f79af1c91b579b214d04
lyang135 <lyang135@ucsc.edu>
2026-03-31 13:04:51 -0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def PrintHelloWorld():
+        #This function prints Hello World
+
+    print("Hello World")
