43e0ef8c51432a17816e07fd1d8ed4ed31baef1e
lyang135 <lyang135@ucsc.edu>
2026-04-08 20:23:27 -0700

    Add files via upload

---
+++ Session_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("name: Leying\npronoun: she/her\n")
+food = "chocolate"
+print(f"My favorite food is {food}")
+movie = "Pearl"
+print(f"My favorite movie is {movie}")
